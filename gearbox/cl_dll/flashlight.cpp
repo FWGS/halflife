0d746614b48e7f4b90f4dc328a5627e4c91c9688
mittorn <mittorn@sibmail.com>
2016-01-18 18:57:32 +0000

    Fix nightvision/flashlight

--- gearbox/cl_dll/flashlight.cpp
+++ gearbox/cl_dll/flashlight.cpp
@@ -28,7 +28,7 @@
 
 
 DECLARE_MESSAGE(m_Flash, FlashBat)
-DECLARE_MESSAGE(m_Flash, Flashlight)
+//DECLARE_MESSAGE(m_Flash, Flashlight)
 
 #define BAT_NAME "sprites/%d_Flashlight.spr"
 
@@ -37,7 +37,7 @@ int CHudFlashlight::Init(void)
 	m_fFade = 0;
 	m_fOn = 0;
 
-	HOOK_MESSAGE(Flashlight);
+//	HOOK_MESSAGE(Flashlight);
 	HOOK_MESSAGE(FlashBat);
 
 	m_iFlags |= HUD_ACTIVE;
